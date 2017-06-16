79687de911f87ec223976556b8a860f0996b4bfe
op7267 <op7267@korea.ac.kr>
2017-06-16 15:38:22 +0900

    ff

--- app/src/main/java/com/mulcam/c901/ari/androidquest/MainActivity.java
+++ app/src/main/java/com/mulcam/c901/ari/androidquest/MainActivity.java
@@ -8,9 +8,15 @@ import android.widget.ListView;
 
 import com.google.gson.internal.LinkedTreeMap;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.*;
 import java.util.List;
 
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,11 +27,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by Jin on 2017-06-14.
  */
 
-public class ActivityMain extends Activity{
+public class MainActivity extends Activity{
     private List<HashMap<String, Object>> list0;
     private List<HashMap<String,Object>> list1;
     private BoardAdapter adapter;
     private ListView main_lv;
+    private OkHttpClient okhttp;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +41,11 @@ public class ActivityMain extends Activity{
     }
     private void setList()
     {
+        okhttp = setInterceptor();
         final Retrofit client = new Retrofit.Builder().baseUrl("http://10.0.3.2:8080/Quest/")
-                        .addConverterFactory(GsonConverterFactory.create()).build();
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .client(okhttp)
+                        .build();
 
         new AsyncTask<Nullable, Nullable, Nullable>()
         {
@@ -72,7 +82,7 @@ public class ActivityMain extends Activity{
 
 
 
-                        adapter = new BoardAdapter(ActivityMain.this, R.layout.board, hash3);
+                        adapter = new BoardAdapter(MainActivity.this, R.layout.board, hash3);
                         main_lv = (ListView) findViewById(R.id.main_lv);
                         main_lv.setAdapter(adapter);
                     }
@@ -86,5 +96,34 @@ public class ActivityMain extends Activity{
             }
         }.execute();
     }
+    private OkHttpClient setInterceptor()
+    {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
 
+                // 헤더를 자유 자재로 변경
+                Request.Builder builder = original.newBuilder();
+                builder.addHeader("Content-Type","application/json; charset=utf-8");
+                builder.addHeader("Accept","application/json; charset=utf-8");
+
+                builder.method(original.method(),original.body());
+                Request request = builder.build();
+
+                okhttp3.Response response = chain.proceed(request);
+
+                // 아래 소스는 response로 오는 데이터가 URLEncode 되어 있을 때
+                // URLDecode 하는 소스 입니다.
+                return response.newBuilder()
+                        .body(ResponseBody.create(response.body().contentType()
+                                , URLDecoder.decode(response.body().string(),"utf-8")))
+                        .build();
+            }
+        });
+
+
+        OkHttpClient okHttp = builder.build();
+        return okHttp;
+    }
 }
