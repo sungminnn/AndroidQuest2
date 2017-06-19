92e2bb1e38bf8ac03c31f04329f5358c714e926a
op7267 <op7267@korea.ac.kr>
2017-06-19 14:16:02 +0900

    slide

--- app/src/main/java/com/mulcam/c901/ari/androidquest/MainActivity.java
+++ app/src/main/java/com/mulcam/c901/ari/androidquest/MainActivity.java
@@ -3,45 +3,61 @@ package com.mulcam.c901.ari.androidquest;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.HashMap;
-import java.util.List;
-
-import okhttp3.OkHttpClient;
 
 /**
  * Created by Jin on 2017-06-14.
  */
 
-public class MainActivity extends FragmentActivity{
-    private List<HashMap<String, Object>> list0;
-    private List<HashMap<String,Object>> list1;
-    private LinearLayout main_view;
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
     private Button todo_btn;
     private Button welldo_btn;
 
     private TodoBoardFragment todoBoard;
     private WelldoBoardFragment welldoBoard;
-
-    private BoardAdapter adapter;
-    private ListView main_lv;
-    private OkHttpClient okhttp;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         todo_btn = (Button) findViewById(R.id.main_todo_btn);
         welldo_btn = (Button) findViewById(R.id.main_welldo_btn);
-        main_view = (LinearLayout) findViewById(R.id.main_view);
 
         todoBoard = new TodoBoardFragment();
         welldoBoard = new WelldoBoardFragment();
@@ -52,12 +68,8 @@ public class MainActivity extends FragmentActivity{
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "해주세요 게시판에 들어오셨습니다", Toast.LENGTH_SHORT).show();
                 Log.i("main","해주세요게시판 in");
-
-
                 FragmentManager fm = getFragmentManager();
                 fm.beginTransaction().replace(R.id.main_view, todoBoard).commit();
-
-
             }
         });
 
@@ -71,7 +83,65 @@ public class MainActivity extends FragmentActivity{
                 fm.beginTransaction().replace(R.id.main_view, welldoBoard).commit();
             }
         });
+
     }
+
+        @Override
+        public void onBackPressed() {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+                    // Inflate the menu; this adds items to the action bar if it is present.
+                    getMenuInflater().inflate(R.menu.main, menu);
+                    return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.nav_camera) {
+                // Handle the camera action
+            } else if (id == R.id.nav_gallery) {
+
+            } else if (id == R.id.nav_slideshow) {
+
+            } else if (id == R.id.nav_manage) {
+
+            } else if (id == R.id.nav_share) {
+
+            } else if (id == R.id.nav_send) {
+
+            }
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
 
 
 }
